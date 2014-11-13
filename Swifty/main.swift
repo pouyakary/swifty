788126d2dffc94b7a43a6f53af42ac2668dd14aa
Pouya Kary <k@arendelle.org>
2014-11-14 00:08:14 +0330

    Initial Commit

---
+++ Swifty/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swifty
+//
+//  Created by Pouya Kary on 11/14/14.
+//  Copyright (c) 2014 Arendelle Language. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+

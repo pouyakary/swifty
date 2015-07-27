34691d23eb44fa829498610440efc2662b438e25
Pouya Kary <k@arendelle.org>
2015-07-27 14:48:32 +0430

    Pi Console moved to KC as well

--- PiConsole.m
+++ PiConsole.m
@@ -8,38 +8,141 @@
 
 #import <Foundation/Foundation.h>
 #include <stdlib.h>
-//#include <editline/readline.h>
 
-/* ----------------------------------------- *
- * :::::::::: P I   C O N S O L E :::::::::: *
- * ----------------------------------------- */
 
-NSString* PiConsoleReadLine() {
+/* ──────────────────────────────────────────────────────────────────────────────────────────────────── *
+ * :::::::::::::::::::::::::::::::::: P I   C O N S O L E   T O O L S ::::::::::::::::::::::::::::::::: *
+ * ──────────────────────────────────────────────────────────────────────────────────────────────────── */
 
+
+//
+// ─── READLINE ───────────────────────────────────────────────────────────────────────────────────────
+//
+
+
+    NSString* PiConsoleReadLine( ) {
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     
-    NSMutableData *data = [NSMutableData data];
-
-    do {
-        char c = getchar();
-        if ([[NSCharacterSet newlineCharacterSet] characterIsMember:(unichar)c]) { break; }
-        [data appendBytes:&c length:sizeof(char)];
+            NSMutableData * data = [ NSMutableData data ];
         
-    } while (true);
-     
-    return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    
-}
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-void PiConsoleClean   () { system("clear"); }
-void PiConsoleReset   () { printf("\033[0m"); }
-void PiConsoleBold    () { printf("\033[1m"); }
-void PiConsoleRed     () { printf("\x1B[31m"); }
-void PiConsoleGreen   () { printf("\x1B[32m"); }
-void PiConsoleYellow  () { printf("\x1B[33m"); }
-void PiConsoleBlue    () { printf("\x1B[34m"); }
-void PiConsoleMagenta () { printf("\x1B[35m"); }
-void PiConsoleCayan   () { printf("\x1B[36m"); }
-void PiConsoleWhite   () { printf("\x1B[37m"); }
+            do {
+            
+                // • • • • •
+            
+                char c = getchar( );
+            
+                // • • • • •
+            
+                if ( [ [ NSCharacterSet newlineCharacterSet ] characterIsMember: ( unichar )c ] ) {
+                
+                    break;
+            
+                } //end of if ( [ [ NSCharacterSet newlineCharacterSet ] characterIsMember: ( unichar )c ] )
+            
+                // • • • • •
+            
+                [ data appendBytes:&c length: sizeof( char ) ];
+        
+            } while ( true );
+        
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+     
+            return [ [ NSString alloc ] initWithData: data encoding: NSUTF8StringEncoding ];
+        
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+    } //end of PiConsoleReadLine( )
+
+
+//
+// ─── CONSOLE COMMENTS ───────────────────────────────────────────────────────────────────────────────
+//
+
+    // • • • • •
+
+    void PiConsoleClean( ) {
+        
+        system( "clear" );
+    
+    }
+
+    // • • • • •
+
+    void PiConsoleReset( ) {
+        
+        printf( "\033[0m" );
+    
+    }
+
+    // • • • • •
+
+    void PiConsoleBold( ) {
+        
+        printf( "\033[1m" );
+    
+    }
+
+    // • • • • •
+
+    void PiConsoleRed( ) {
+        
+        printf( "\x1B[31m" );
+    
+    }
+
+    // • • • • •
+
+    void PiConsoleGreen( ) {
+        
+        printf( "\x1B[32m" );
+    
+    }
+
+    // • • • • •
+
+    void PiConsoleYellow( ) {
+        
+        printf( "\x1B[33m" );
+    
+    }
+
+    // • • • • •
+
+    void PiConsoleBlue( ) {
+        
+        printf( "\x1B[34m" );
+    
+    }
+
+    // • • • • •
+
+    void PiConsoleMagenta( ) {
+        
+        printf( "\x1B[35m" );
+    
+    }
+
+    // • • • • •
+
+    void PiConsoleCayan( ) {
+        
+        printf( "\x1B[36m" );
+    
+    }
+
+    // • • • • •
+
+    void PiConsoleWhite( ) {
+        
+        printf( "\x1B[37m" );
+    
+    }
+
+
+// ────────────────────────────────────────────────────────────────────────────────────────────────────
 
 
 
